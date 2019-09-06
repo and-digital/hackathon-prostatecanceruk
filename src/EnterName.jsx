c2eb58988214e1b173cc6dc3d0eb1ba605f07903
David Garvie <garvie88@gmail.com>
2019-09-06 13:53:31 +0100

    Update some styles

--- src/EnterName.jsx
+++ src/EnterName.jsx
@@ -6,12 +6,12 @@ const EnterName = ({ eventHandler }) => {
 
   return (
     <div className="enterNameContainer">
-      <div className="imgContainer">
+      <div>
         <img src={logo} />
+        <h1>Who are you creating the page for?</h1>
+        <input placeholder="The persons name" className="input" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <button className="button" onClick={() => eventHandler(name)}>Submit Name</button>
       </div>
-      <h1>Who are you creating the page for?</h1>
-      <input placeholder="The persons name" className="input" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      <button className="button" onClick={() => eventHandler(name)}>Submit Name</button>
     </div>
   )
 }

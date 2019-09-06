fc333b38ff26892fe9bae30eabc4cc628e59def1
David Garvie <garvie88@gmail.com>
2019-09-06 13:00:57 +0100

    Style updates

--- src/ButtonsHolder/ButtonsHolder.js
+++ src/ButtonsHolder/ButtonsHolder.js
@@ -3,9 +3,8 @@ import React from 'react';
 const ButtonsHolder = ({clickHandler}) => {
   return (
     <div className="buttons-holder">
-      <h1>Select a Button</h1>
-      <button className="option-btn" onClick={clickHandler}>Music</button>
-      <button className="option-btn" onClick={clickHandler}>Cooking</button>
+      <button className="button" onClick={clickHandler}>Music</button>
+      <button className="button" onClick={clickHandler}>Cooking</button>
     </div>
   );
 };

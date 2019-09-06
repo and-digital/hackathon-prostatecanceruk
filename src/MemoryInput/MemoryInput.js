e1fed7354a85cfed489228b3f387cf644d7ee46d
Billvamangal Praviendran <bpraviendran@and.digital>
2019-09-06 14:42:34 +0100

    Memory input component added

---
+++ src/MemoryInput/MemoryInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MemoryInput = () => {
+  return (
+    <div className="input-container">
+      <p>Tell us more about your memory</p>
+      <form action="" >
+        <textarea type="text"/>
+        <button className="button">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default MemoryInput;

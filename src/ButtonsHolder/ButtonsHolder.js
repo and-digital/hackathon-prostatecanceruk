711020b632ad6df08d6cef4c48efbe91872eb175
Billvamangal Praviendran <bpraviendran@and.digital>
2019-09-06 12:22:06 +0100

    buttons holder component added

---
+++ src/ButtonsHolder/ButtonsHolder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ButtonsHolder = () => {
+  return (
+    <div className="buttons-holder">
+      <h1>Select a Button</h1>
+    </div>
+  );
+};
+
+export default ButtonsHolder;

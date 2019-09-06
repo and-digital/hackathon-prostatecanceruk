b364a436676d1619210bbba6e3894b4b77f4dd80
David Garvie <garvie88@gmail.com>
2019-09-06 13:49:46 +0100

    Add logo and make enter name page black

--- src/EnterName.jsx
+++ src/EnterName.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import logo from './assets/logo.jpg';
 
 const EnterName = ({ eventHandler }) => {
   const [name, setName] = useState(''); 
 
   return (
     <div className="enterNameContainer">
+      <div className="imgContainer">
+        <img src={logo} />
+      </div>
       <h1>Who are you creating the page for?</h1>
-      <input className="input" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <input placeholder="The persons name" className="input" type="text" value={name} onChange={(e) => setName(e.target.value)} />
       <button className="button" onClick={() => eventHandler(name)}>Submit Name</button>
     </div>
   )

ffdd19ac705547242b0e1056746c94bc1aceb584
David Garvie <garvie88@gmail.com>
2019-09-06 12:48:45 +0100

    Get id from url

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React from 'react';
+import queryString from 'query-string';
 import Donate from './Donate';
 import Heading from './Heading';
 import SocialLinks from './SocialLinks';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Heading name="David" />
-      <Donate />
-      <SocialLinks />
-    </div>
-  );
+const names = {
+  1: 'Pravien',
+  2: 'David',
+}
+
+function getID() { 
+  const { id } = queryString.parse(window.location.search);
+  return id;
+}
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      id: null,
+      name: null,
+    }
+  }
+
+  componentDidMount() {
+    const id = getID();
+
+    this.setState({
+      id,
+      name: names[id],
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Heading name={this.state.name} />
+        <Donate />
+        <SocialLinks />
+      </div>
+    );
+  }
 }
 
 export default App;
